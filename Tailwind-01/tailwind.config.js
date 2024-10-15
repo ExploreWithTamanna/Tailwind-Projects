c6c614d1d20bbc8be95a5ce3a2007a32a18b54f0
Tamanna Binte Abdullah <tbinteabdullah@gmail.com>
2024-10-15 11:51:51 +0600

    project-completed

---
+++ Tailwind-01/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    fontFamily:{
+      "display":["Edu AU VIC WA NT Guides","sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
